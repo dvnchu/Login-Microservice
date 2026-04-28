6dbb9c7308737fbad73f4db7531393d0e2c6acfe
dculini <dculini@gmail.com>
2026-04-28 18:33:59 -0300

    Sqlalchemy engine done

---
+++ service.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+# Carga archivo .env
+load_dotenv()
+
+# Rescatamos las variables que definimos
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = "login_db"
+
+
+DB_HOST = "localhost"
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
